9925702d3d0d9c681c446766ad671b6f01b69111
Muhammed sinan.p <psinan32@gmail.com>
2025-08-20 08:05:59 +0530

    97% over

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./rooms/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
